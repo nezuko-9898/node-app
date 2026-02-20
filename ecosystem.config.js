d5c940de32aac829625c6218f4976034ffc744de
Mali Pravin <malipravin5545@gmail.com>
2026-02-20 11:20:09 +0530

    new commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,36 @@
 module.exports = {
-    apps: [
-      {
-        name: "node-app",
-        script: "app.js",
-        exec_mode: "cluster",
-        instances: "max",
-  
-        env_development: {
-          NODE_ENV: "development",
-          PORT: 7000
-        },
-  
-        env_staging: {
-          NODE_ENV: "staging",
-          PORT: 4000
-        },
-  
-        env_production: {
-          NODE_ENV: "production",
-          PORT: 80
-        }
+  apps: [
+
+    {
+      name: "dev-app",
+      script: "app.js",
+      instances: 1,
+      env_development: {
+        NODE_ENV: "development",
+        PORT: 7000
       }
-    ]
-  };
-  
+    },
+
+    {
+      name: "staging-app",
+      script: "app.js",
+      instances: 1,
+      env_staging: {
+        NODE_ENV: "staging",
+        PORT: 4000
+      }
+    },
+
+    {
+      name: "prod-app",
+      script: "app.js",
+      instances: "max",
+      exec_mode: "cluster",
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 80
+      }
+    }
+
+  ]
+}
